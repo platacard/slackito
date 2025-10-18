d98a4774196bc3c4770220f6a5235732382ee742
Max K. <max.kraev@gmail.com>
2025-10-18 15:08:20 +0200

    chore: bump cronista

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/platacard/cronista.git", from: "1.0.2"),
+        .package(url: "https://github.com/platacard/cronista.git", from: "1.0.3"),
     ],
     targets: [
         .target(
